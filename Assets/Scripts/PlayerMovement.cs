3e5dd7ca2da600af7268004ada033cbb10a60570
Shahab Rostami <shbrostami@gmail.com>
2017-08-06 21:41:58 +0100

    Simple collision triggers

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,5 +21,13 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+            other.gameObject.SetActive(false);
+        
     }
 }
